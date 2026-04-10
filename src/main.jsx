be8ebb118c8df693b340e4e4c67be21a14e47c45
riverimenemessadh <sarahimenemessadh@gmail.com>
2026-04-10 18:28:20 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Zpp from './Zpp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Zpp />
   </StrictMode>,
 )
